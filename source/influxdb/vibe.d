545f74f115073f1d2ec254e89ab2e73a275f622b
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-06-13 14:04:37 +0200

    Switch from vibe:http to requests library

--- source/influxdb/vibe.d
+++ source/influxdb/vibe.d
@@ -9,87 +9,52 @@
 */
 module influxdb.vibe;
 
+import requests : Request, Response, urlEncoded;
+
 
 ///
 void manage(in string url, in string str) {
-    vibePostQuery(url, "q=" ~ str);
+    httpPostRequest(url ~ "/query", ["q": str]);
 }
 
 ///
 string query(in string url, in string db, in string query) {
-    return vibeGet(url, db, query);
+    return httpGetRequest(url ~ "/query", ["db": db, "q": query]);
 }
 
 ///
 void write(in string url, in string db, in string line) {
-    vibePostWrite(url, db, line);
-}
-
-private string vibePostQuery(in string url,
-                             in string query,
-                             in string file = __FILE__,
-                             in size_t line = __LINE__) {
-    return vibeHttpRequest(url ~ "/query",
-                           cast(ubyte[])query.urlEncode,
-                           file,
-                           line);
-}
-
-private string vibePostWrite(in string url, in string db, in string str,
-                             in string file = __FILE__, in size_t line = __LINE__) {
-    return vibeHttpRequest(url ~ "/write?db=" ~ db, cast(ubyte[])str, file, line);
+    httpPostRequest(url ~ "/write?db=" ~ urlEncoded(db), cast(ubyte[]) line);
 }
 
 
-private string vibeGet(in string url, in string db, in string arg,
-                          in string file = __FILE__, in size_t line = __LINE__) {
-
-    import std.algorithm: map;
-    import std.array: array, join;
-    import std.range: chain;
-
-    const fullUrl = url ~ "/query?" ~ ["db=" ~ db, "q=" ~ arg].map!urlEncode.array.join("&");
-    return vibeHttpRequest(fullUrl, [], file, line);
+private string httpGetRequest(in string url,
+                              string[string] queryParams,
+                              in string file = __FILE__,
+                              in size_t line = __LINE__) {
+    auto res = Request().get(url, queryParams);
+    return processResponse(res, file, line);
 }
 
-private string vibeHttpRequest(in string url,
+private string httpPostRequest(in string url,
+                               string[string] postParams,
+                               in string file = __FILE__,
+                               in size_t line = __LINE__) {
+    auto res = Request().post(url, postParams);
+    return processResponse(res, file, line);
+}
+
+private string httpPostRequest(in string url,
                                in ubyte[] data,
                                in string file = __FILE__,
                                in size_t line = __LINE__) {
-
-    import vibe.http.client: requestHTTP, HTTPMethod;
-    import vibe.stream.operations: readAllUTF8;
-
-    string ret;
-
-    requestHTTP(url,
-                (scope req) {
-
-                    req.contentType= "application/x-www-form-urlencoded";
-                    if(data.length) {
-                        req.method = HTTPMethod.POST;
-                        req.writeBody(data);
-                    }
-
-                },
-                (scope res) {
-                    ret = res.bodyReader.readAllUTF8;
-                    if(res.statusCode < 200 || res.statusCode > 299)
-                        throw new Exception(ret, file, line);
-                }
-        );
-
-    return ret;
-
+    auto res = Request().post(url, data, "application/x-www-form-urlencoded");
+    return processResponse(res, file, line);
 }
 
-///
-string urlEncode(in string str) {
-    import vibe.textfilter.urlencode: filterURLEncode;
-    import std.array: appender;
-
-    auto output = appender!(char[]);
-    const allowedChars = "=";
-    filterURLEncode(output, str, allowedChars);
-    return cast(string)output.data;
+private string processResponse(Response response, in string file, in size_t line) {
+    const ret = response.responseBody.toString();
+    if (response.code < 200 || response.code > 299)
+        throw new Exception(ret, file, line);
+    return ret;
 }
